96bf502bb1f3933022a25f5e909e6ed0d0b04b41
Carl Barbee <carlb15@vt.edu>
2014-04-07 18:56:29 -0400

    deleting files

--- PIC_Framework_3.X/build/UseXC8/production/_ext/1360937237/uart_thread.p1.d
+++
@@ -1,8 +0,0 @@
- build/UseXC8/production/_ext/1360937237/uart_thread.d  \
- build/UseXC8/production/_ext/1360937237/uart_thread.p1:  \
- ../src/uart_thread.c  \
-../src/debug.h  \
-../src/my_uart.h  \
-../src/maindefs.h  \
-../src/messages.h  \
-../src/uart_thread.h 
